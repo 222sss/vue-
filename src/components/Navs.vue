ec753973912669779e9699bd816ac43bf706f3a4
xiaoranHome <1564383237@qq.com>
2019-05-04 16:32:15 +0800

    导航

---
+++ src/components/Navs.vue
@@ -0,0 +1,73 @@
+/**导航 */
+<template>
+  <div v-bind:class="['nav-div', 'w100', 'h100', diyClass]">
+    <div
+      v-for="nav of navs"
+      v-bind:key="nav.id"
+      v-bind:class="['nav', 'w100', { navActive: active == nav.router }]"
+      @click="change(nav.router)"
+    >
+      {{ nav.text }}
+    </div>
+  </div>
+</template>
+
+<script>
+import navsList from "utils/data";
+
+export default {
+  name: "Navs",
+  props: {
+    diyClass: {
+      type: String,
+      default: ""
+    }
+  },
+  data: function() {
+    return {
+      active: "aaa"
+    };
+  },
+  computed: {
+    navs: function() {
+      let navs = [];
+      navs = navsList.basisPlatformNavs;
+      return navs;
+    }
+  },
+  methods: {
+    change: function(router) {
+      this.$router.push({ path: `/${router}` });
+      this.active = router;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "assets/css/common.scss";
+
+.nav-div {
+  @include flex;
+  @include flex-column;
+  @include flex-primary-axis-center;
+  @include flex-secondary-axis-center;
+
+  .nav {
+    @include bg-color(#002890);
+    color: $font-color-1;
+    border: 1px solid #013d85;
+    border-radius: 20px;
+    font-size: 15px;
+    height: 35px;
+    margin-bottom: 12px;
+    line-height: 35px;
+    text-align: center;
+  }
+
+  .navActive {
+    @include bg-color(#003bd9);
+    color: $color-fff;
+  }
+}
+</style>
--- src/views/BasisPlatform.vue
+++ src/views/BasisPlatform.vue
@@ -1,13 +1,23 @@
 /**基础平台主页 */
 <template>
   <div class="basisPlatform w100 h100">
-    basisPlatform
+    <div class="navs h100">
+      <navs></navs>
+    </div>
+    <div class="content h100">
+      123
+    </div>
   </div>
 </template>
 
 <script>
+import Navs from "components/Navs";
+
 export default {
-  name: "BasisPlatform"
+  name: "BasisPlatform",
+  components: {
+    Navs
+  }
 };
 </script>
 
@@ -15,7 +25,18 @@ export default {
 @import "assets/css/common.scss";
 
 .basisPlatform {
-  background: red;
+  @include flex;
   overflow: hidden;
+
+  .navs {
+    width: 117px;
+    margin-right: 30px;
+    border: 1px solid red;
+  }
+
+  .content {
+    flex: 1;
+    background: red;
+  }
 }
 </style>
